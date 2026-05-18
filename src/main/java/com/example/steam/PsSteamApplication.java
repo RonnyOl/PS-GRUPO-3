0eb6d5a6e0c406a865ca0a3bc556c1ffddf61d69
Ronnyzxzz <ronnyolmos16@gmail.com>
2026-05-18 20:29:51 -0300

    Primer commit

---
+++ src/main/java/com/example/steam/PsSteamApplication.java
@@ -0,0 +1,13 @@
+package com.example.steam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsSteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsSteamApplication.class, args);
+    }
+
+}
